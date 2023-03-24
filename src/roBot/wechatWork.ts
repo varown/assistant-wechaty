6ebe74c48e24d321b00422abc5bcc18d3ed50617
mengpeng <mengpeng@app315.net>
2023-03-24 16:41:27 +0800

    fix: undici replace axios

--- src/roBot/wechatWork.ts
+++ src/roBot/wechatWork.ts
@@ -1,20 +1,35 @@
-import axios from "axios";
+import { Client } from "undici";
 import { WEWORK_BOT_KEY } from "../config.js";
-
 // 企业微信机器人配置
-
 const weWorkBotKey = process.env.WEWORK_BOT_KEY || WEWORK_BOT_KEY
 
+
 export async function sendWechatWorkMessage(message: any) {
-  const response = await axios.post(`https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${weWorkBotKey}`, {
-    msgtype: 'text',
-    text: {
-      content: message
+  const client = new Client('https://qyapi.weixin.qq.com');
+
+  try {
+    const { statusCode, body } = await client.request({
+      path: `/cgi-bin/webhook/send?key=${weWorkBotKey}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        msgtype: 'text',
+        text: {
+          content: message
+        }
+      }),
+    });
+
+    const response = JSON.parse(await body.text());
+
+    if (statusCode === 200 && response.errcode === 0) {
+      console.log('企业微信通知发送成功！');
+    } else {
+      console.error('企业微信通知发送失败：', response.errmsg);
     }
-  });
-  if (response.data.errcode === 0) {
-    console.log('企业微信通知发送成功！');
-  } else {
-    console.error('企业微信通知发送失败：', response.data.errmsg);
+  } finally {
+    await client.close();
   }
 };
